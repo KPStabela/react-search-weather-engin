5aa05349e37501687cf72c226700c5725d568048
KP-Stabela <159870825+KP-Stabela@users.noreply.github.com>
2024-03-18 06:57:39 +0200

    Removed some unwanted css

---
+++ src/Weather-App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Weather-App.css"
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <h1>Orkney</h1>
+            <p>
+                Sunday 12:00, Partly Cloudy
+            </p>
+        </div>
+    );
+}
